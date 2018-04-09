fec719f1d82d0b48e0fecf0cd60b636117bad96d
clam <coderzww@gmail.com>
2018-04-09 23:40:20 +0800

    can i use

--- src/extension.ts
+++ src/extension.ts
@@ -1,29 +1,79 @@
-'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+const open = require("opn");
+const caniuseAPI = require("caniuse-api");
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+const BROWSERS = { chrome: "CR", firefox: "FF", ie: "IE", edge: "Edge", opera: "OP", safari: "SF" };
 export function activate(context: vscode.ExtensionContext) {
-
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "browser-compatibility" is now active!');
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+    const status = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left);
+    const dEditor = vscode.window.onDidChangeActiveTextEditor(() => {
+        status.hide();
+    });
+    const dSelection = vscode.window.onDidChangeTextEditorSelection(() => {
+        status.hide();
     });
 
-    context.subscriptions.push(disposable);
+    const eCaniuse = vscode.commands.registerCommand("extension.caniuse", () => {
+        let statusInfo = caniuse();
+        status.text = statusInfo === "" ? `Unkown Prop: Search in the browser` : statusInfo;
+        status.command = "extension.browse";
+        status.tooltip = "Click to search in caniuse website";
+        status.show();
+    });
+    const eBrowser = vscode.commands.registerCommand("extension.browse", () => {
+        open(`http://caniuse.com/#search=${getSelectedText()}`);
+    });
+    const tscan2es5 = vscode.commands.registerCommand("extension.tscan2es5", () => {});
+
+    context.subscriptions.push(status, dEditor, dSelection, eCaniuse, eBrowser, tscan2es5);
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {
+function getSelectedText() {
+    const aEditor = vscode.window.activeTextEditor;
+    return aEditor ? aEditor.document.getText(aEditor.selection) : "";
+}
+
+function caniuse() {
+    const sText = getSelectedText();
+    if (sText === "") {
+        return;
+    }
+
+    let ret = {};
+    try {
+        // https://github.com/Nyalab/caniuse-api#caniusegetsupportfeature
+        ret = caniuseAPI.getSupport(sText);
+    } catch (e) {
+        const sugs = caniuseAPI.find(sText);
+        if (sugs && sugs.length > 0) {
+            vscode.window.showQuickPick(sugs).then(picked => {
+                try {
+                    ret = caniuseAPI.getSupport(picked);
+                } catch (ex) {
+                    throw ex;
+                }
+            });
+        }
+    }
+
+    return assembleInfo(ret);
+}
+
+function assembleInfo(support) {
+    let info = "";
+    if (support && support !== {}) {
+        Object.keys(BROWSERS).forEach(b => {
+            /**
+             * y: Since which browser version the feature is available
+             * n: Up to which browser version the feature is unavailable
+             * a: Up to which browser version the feature is partially supported
+             * x: Up to which browser version the feature is prefixed
+             */
+            if (support[b].hasOwnProperty("y")) {
+                info = `${info} ${BROWSERS[b]} ( ${support[b].y} ) `;
+            }
+        });
+    }
+    return info;
 }
